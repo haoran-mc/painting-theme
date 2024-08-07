447daae32ad3da3074dfece253517629543c1326
haoran <haoran.mc@outlook.com>
2024-08-07 10:29:49 +0800

    monokai remove credits and version prompt

--- monokai-theme.el
+++ monokai-theme.el
@@ -24,31 +24,7 @@
 ;; A port of the popular Textmate theme Monokai for Emacs 24, built on top
 ;; of the new built-in theme support in Emacs 24.
 ;;
-;;; Credits:
-;;
-;; Wimer Hazenberg created the original theme.
-;; - http://www.monokai.nl/blog/2006/07/15/textmate-color-theme/
-;;
-;; Bozhidar Batsov created zenburn-theme.el and solarized-theme.el
-;;  on which this file is based.
-;; - https://github.com/bbatsov/zenburn-emacs
-;;
-;; Color Scheme Designer 3 for complementary colours.
-;; - http://colorschemedesigner.com/
-;;
-;; Xterm 256 Color Chart
-;; - https://upload.wikimedia.org/wikipedia/en/1/15/Xterm_256color_chart.svg
-;;
-;; K. Adam Christensen for his personal monokai theme that addresses 256 colours.
-;; - https://github.com/pope/personal/blob/master/etc/emacs.d/monokai-theme.el
-;;
-;; Thomas Frössman for his work on solarized-emacs.
-;; - http://github.com/bbatsov/solarized-emacs
-;;
 ;;; Code:
-
-(unless (>= emacs-major-version 24)
-  (error "The monokai theme requires Emacs 24 or later!"))
 
 (deftheme monokai "The Monokai colour theme")
 
