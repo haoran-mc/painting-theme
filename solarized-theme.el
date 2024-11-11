c8b6fc6a20ad14e93809730bb4e258e9e5f6ee15
haoran <haoran.mc@outlook.com>
2024-11-11 20:27:05 +0800

    solarized remove 256-color and some cars

--- solarized-theme.el
+++ solarized-theme.el
@@ -290,14 +290,6 @@
     (cider-deprecated-face     :background (doom-blend bg yellow 0.8))
     (cider-instrumented-face   :background (doom-blend bg red 0.8))
     (cider-traced-face         :background (doom-blend bg cyan 0.8))
-    (cider-error-highlight-face
-     `((((supports :underline (:style wave)))
-        (:inherit unspecified :underline (:style wave :color ,(car error))))
-       (t (:inherit font-lock-warning-face :underline t))))
-    (cider-warning-highlight-face
-     `((((supports :underline (:style wave)))
-        (:underline (:style wave :color ,(car warning)) :inherit unspecified))
-       (t (:inherit font-lock-warning-face :underline (:color ,(car warning))))))
     (cider-test-failure-face :background (doom-blend bg error 0.7))
     (cider-test-error-face   :background orange)
     (cider-test-success-face :foreground base0 :background green)
@@ -818,11 +810,6 @@
     ;; Emacs <25 compatibility
     (smerge-mine  :inherit 'smerge-upper)
     (smerge-other :inherit 'smerge-lower)
-    ;;;; spell-fu
-    (spell-fu-incorrect-face
-     `((((supports :underline (:style wave)))
-        (:underline (:style wave :color ,(car error))))
-       (t (:inherit error :underline t))))
     ;;;; stripe-buffer
     (stripe-highlight :background base5)
     ;;;; symbol-overlay
@@ -1062,12 +1049,14 @@
        (cond ((keywordp (car face-body))
               (let ((real-attrs face-body)
                     defs)
-                (if (doom-themes--colors-p real-attrs)
-                    (dolist (cl doom--min-colors `(list ,@(nreverse defs)))
-                      (push `(list '((class color) (min-colors ,cl))
-                                   (list ,@(doom-themes--colorize real-attrs cl)))
-                            defs))
-                  `(list (list 't (list ,@real-attrs))))))
+                ;; (if (doom-themes--colors-p real-attrs)
+                ;; (dolist (cl doom--min-colors `(list ,@(nreverse defs)))
+                ;; (push `(list '((class color) (min-colors ,cl))
+                ;; (list ,@(doom-themes--colorize real-attrs cl)))
+                ;; defs))
+                `(list (list 't (list ,@real-attrs)))
+                ;; )
+                ))
 
              ((memq (car-safe (car face-body)) '(quote backquote \`))
               (car face-body))))))
@@ -1212,41 +1201,41 @@ Faces in EXTRA-FACES override the default faces."
                 "A light theme inspired by Solarized light"
 
                 ;; name        default   256       16
-                ((bg         '("#FDF6E3" "#FDF6E3" "white"        ))
-                 (fg         '("#556b72" "#556b72" "black"        ))
+                ((bg         "#FDF6E3")
+                 (fg         "#556b72")
 
                  ;; These are off-color variants of bg/fg, used primarily for `solaire-mode',
                  ;; but can also be useful as a basis for subtle highlights (e.g. for hl-line
                  ;; or region), especially when paired with the `doom-darken', `doom-lighten',
                  ;; and `doom-blend' helper functions.
-                 (bg-alt     '("#EEE8D5" "#EEE8D5" "white"        ))
-                 (fg-alt     '("#7B8787" "#7B8787" "brightwhite"  ))
+                 (bg-alt     "#EEE8D5")
+                 (fg-alt     "#7B8787")
 
                  ;; These should represent a spectrum from bg to fg, where base0 is a starker
                  ;; bg and base8 is a starker fg. For example, if bg is light grey and fg is
                  ;; dark grey, base0 should be white and base8 should be black.
-                 (base0      '("#FFFBF0" "#FFFBF0" "white"        ))
-                 (base1      '("#FCF8ED" "#FCF8ED" "brightblack"  ))
-                 (base2      '("#FCF7E8" "#FCF7E8" "brightblack"  ))
-                 (base3      '("#F2E6CE" "#F2E6CE" "brightblack"  ))
-                 (base4      '("#E1DBCD" "#E1DBCD" "brightblack"  ))
-                 (base5      '("#D6D6D6" "#D6D6D6" "brightblack"  ))
-                 (base6      '("#96A7A9" "#96A7A9" "brightblack"  ))
-                 (base7      '("#788484" "#788484" "brightblack"  ))
-                 (base8      '("#626C6C" "#626C6C" "black"        ))
+                 (base0      "#FFFBF0")
+                 (base1      "#FCF8ED")
+                 (base2      "#FCF7E8")
+                 (base3      "#F2E6CE")
+                 (base4      "#E1DBCD")
+                 (base5      "#D6D6D6")
+                 (base6      "#96A7A9")
+                 (base7      "#788484")
+                 (base8      "#626C6C")
 
                  (grey       base4)
-                 (red        '("#dc322f" "#dc322f" "red"          ))
-                 (orange     '("#cb4b16" "#cb4b16" "brightred"    ))
-                 (green      '("#859900" "#859900" "green"        ))
-                 (teal       '("#35a69c" "#35a69c" "brightgreen"  ))
-                 (yellow     '("#b58900" "#b58900" "yellow"       ))
-                 (blue       '("#268bd2" "#268bd2" "brightblue"   ))
-                 (dark-blue  '("#3F88AD" "#3F88AD" "blue"         ))
-                 (magenta    '("#d33682" "#d33682" "magenta"      ))
-                 (violet     '("#6c71c4" "#6c71c4" "brightmagenta"))
-                 (cyan       '("#2aa198" "#2aa198" "brightcyan"   ))
-                 (dark-cyan  '("#204052" "#204052" "cyan"         ))
+                 (red        "#dc322f")
+                 (orange     "#cb4b16")
+                 (green      "#859900")
+                 (teal       "#35a69c")
+                 (yellow     "#b58900")
+                 (blue       "#268bd2")
+                 (dark-blue  "#3F88AD")
+                 (magenta    "#d33682")
+                 (violet     "#6c71c4")
+                 (cyan       "#2aa198")
+                 (dark-cyan  "#204052")
 
                  ;; face categories -- required for all themes
                  (highlight      blue)
@@ -1264,7 +1253,8 @@ Faces in EXTRA-FACES override the default faces."
                  (strings        cyan)
                  (variables      blue)
                  (numbers        violet)
-                 (region         `(,(doom-darken (car bg-alt) 0.1) ,@(doom-darken (cdr base0) 0.1)))
+                 ;;(region         `(,(doom-darken (car bg-alt) 0.1) ,@(doom-darken (cdr base0) 0.1)))
+                 (region         bg-alt) ;; TODO
                  (error          red)
                  (warning        yellow)
                  (success        green)
